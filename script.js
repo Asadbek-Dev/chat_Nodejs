2a6bc3a4c9da1d905606e1c24ed21551de2955da
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-09-22 22:23:15 +0500

    completed

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-const socket =io('http://localhost:3000')
-const messageForm=document.getElementById('send-container');
-const messageInput=document.getElementById('message-input');
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
 
-socket.on('chat-message', data=>{
-    console.log(data);
+const name = prompt('What is your name?')
+appendMessage('You joined')
+socket.emit('new-user', name)
+
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
 })
 
-messageForm.addEventListener('submit',e=>{
-    e.preventDefault();
-    const message=messageInput.value
-    socket.emit('send-chat-message',message)
-    messageInput.value=''
+socket.on('user-connected', name => {
+  appendMessage(`${name} connected`)
 })
+
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} disconnected`)
+})
+
+messageForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const message = messageInput.value
+  appendMessage(`You: ${message}`)
+  socket.emit('send-chat-message', message)
+  messageInput.value = ''
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
+}
